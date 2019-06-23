f4df021925e1f2a4ad357ed91f5a4b09ed688637
Mulubwa17 <mulubwac@gmail.com>
2019-06-24 00:08:46 +0200

    modified todo.model.js

--- todo-BE/todo.model.js
+++ todo-BE/todo.model.js
@@ -12,4 +12,7 @@ const Schema = mongoose.Schema;let Todo = new Schema({
     todo_completed: {
         type: Boolean
     }
-});module.exports = mongoose.model('Todo', Todo);
+
+        
+});
+module.exports = mongoose.model('Todo', Todo);
