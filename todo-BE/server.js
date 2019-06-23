bb423751a39aa239c5e9eef6eaed2d93cd7d046b
mcdee92 <40967802+mcdee92@users.noreply.github.com>
2019-06-23 16:48:37 +0200

    format server.js

--- todo-BE/server.js
+++ todo-BE/server.js
@@ -8,7 +8,8 @@ const PORT = 4000;let Todo = require('./todo.model');app.use(cors());
 app.use(bodyParser.json());mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
-})todoRoutes.route('/').get(function(req, res) {
+});
+todoRoutes.route('/').get(function(req, res) {
     Todo.find(function(err, todos) {
         if (err) {
             console.log(err);
@@ -16,12 +17,14 @@ const connection = mongoose.connection;connection.once('open', function() {
             res.json(todos);
         }
     });
-});todoRoutes.route('/:id').get(function(req, res) {
+});
+todoRoutes.route('/:id').get(function(req, res) {
     let id = req.params.id;
     Todo.findById(id, function(err, todo) {
         res.json(todo);
     });
-});todoRoutes.route('/update/:id').post(function(req, res) {
+});
+todoRoutes.route('/update/:id').post(function(req, res) {
     Todo.findById(req.params.id, function(err, todo) {
         if (!todo)
             res.status(404).send("data is not found");
@@ -36,7 +39,8 @@ const connection = mongoose.connection;connection.once('open', function() {
                 res.status(400).send("Update not possible");
             });
     });
-});todoRoutes.route('/add').post(function(req, res) {
+});
+todoRoutes.route('/add').post(function(req, res) {
     let todo = new Todo(req.body);
     todo.save()
         .then(todo => {
@@ -45,7 +49,8 @@ const connection = mongoose.connection;connection.once('open', function() {
         .catch(err => {
             res.status(400).send('adding new todo failed');
         });
-});app.use('/todos', todoRoutes);
+});
+app.use('/todos', todoRoutes);
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
